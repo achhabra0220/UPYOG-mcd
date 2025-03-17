4aa8985ca874aacbee4d7162b9399f4362f78d8c
achhabra0220 <129250131+achhabra0220@users.noreply.github.com>
2025-03-17 16:57:00 +0530

    Update Search.js
    added code as per Parvesh changes in niuatt

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/ASSET/Search.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/molecules/ASSET/Search.js
@@ -2,12 +2,17 @@
 import { ASSETService } from "../../elements/ASSET";
 
 const convertTimestampToDate = (timestamp) => {
-  const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+  // Agar timestamp 13 digits ka hai, to yeh milliseconds mein hai
+  const adjustedTimestamp = timestamp.toString().length === 13 ? timestamp / 1000 : timestamp;
+
+  // Timestamp ko date object mein convert karo
+  const date = new Date(adjustedTimestamp * 1000); // Convert seconds to milliseconds
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+  const day = String(date.getDate()).padStart(2, "0");
+
   return `${year}-${month}-${day}`;
-}
+};
 
 const getData = (res, combinedData) => {
 
@@ -29,7 +34,7 @@ const getData = (res, combinedData) => {
 
   // Add more rows to the rows array for Static 
   rows.push({ title: "AST_MODE_OF_POSSESSION_OR_ACQUISITION", value: res?.modeOfPossessionOrAcquisition });
-  rows.push({ title: "AST_INVOICE_DATE", value:convertTimestampToDate( res?.invoiceDate) });
+  rows.push({ title: "AST_INVOICE_DATE", value: convertTimestampToDate(res?.invoiceDate) });
   rows.push({ title: "AST_INVOICE_NUMBER", value: res?.invoiceNumber });
   rows.push({ title: "AST_PURCHASE_DATE", value: convertTimestampToDate(res?.purchaseDate) });
   rows.push({ title: "AST_PURCHASE_ORDER", value: res?.purchaseOrderNumber });
@@ -38,6 +43,7 @@ const getData = (res, combinedData) => {
   rows.push({ title: "AST_PURCHASE_COST", value: res?.purchaseCost });
   rows.push({ title: "AST_ACQUISITION_COST", value: res?.acquisitionCost });
   rows.push({ title: "AST_BOOK_VALUE", value: res?.bookValue });
+  rows.push({ title: "AST_ORIGINAL_VALUE", value: res?.originalBookValue });
   return rows
 }
 const extractValue = (key) => {
@@ -59,8 +65,8 @@ export const ASSETSearch = {
     const response = await ASSETService.search({ tenantId, filters });
     return response.Assets[0];
   },
-  RegistrationDetails: ({ Assets: response, combinedData, t , applicationDetails}) => {
-console.log('first:= ', applicationDetails);
+  RegistrationDetails: ({ Assets: response, combinedData, t, applicationDetails, maintenanceList, disposalList }) => {
+
     const formatDate = (epochTime) => {
       if (!epochTime) return '';
       const date = new Date(epochTime);
@@ -70,16 +76,40 @@ console.log('first:= ', applicationDetails);
         year: 'numeric'
       }).replace(/\//g, '/');
     };
-      const slotlistRows = applicationDetails?.DepreciationDetails?.map((row) => (
-        [
-          row.fromDate,
-          row.toDate,
-          row.depreciationValue,
-          row.bookValue,
-          row.rate,
-          row.oldBookValue
-        ]
-      )) || [];
+    const slotlistRows = applicationDetails?.DepreciationDetails?.map((row) => (
+      [
+        row.fromDate,
+        row.toDate,
+        row.depreciationValue,
+        row.bookValue,
+        row.rate,
+        row.depreciationMethod,
+        row.oldBookValue,
+      ]
+    )) || [];
+    
+    const maintenanceListRows = maintenanceList?.AssetMaintenance?.map((row) => (
+      [
+        convertTimestampToDate(row.assetMaintenanceDate),
+        row.vendor,
+        row.warrantyStatus,
+        row.costOfMaintenance,
+        row.maintenanceCycle,
+        row.documents,
+        {editButton: true, data: row}
+      ]
+    )) || [];
+
+    const disposalListListRows = disposalList?.AssetDisposals?.map((row) => (
+      [
+        convertTimestampToDate(row.disposalDate),
+        row.reasonForDisposal,
+        row.glCode,
+        row.assetDisposalStatus,
+        row.currentAgeOfAsset
+      ]
+    )) || [];
+
     return [
 
       {
@@ -96,8 +126,9 @@ console.log('first:= ', applicationDetails);
           { title: "AST_CATEGORY_SUB_CATEGORY", value: response?.assetSubCategory },
           { title: "AST_NAME", value: response?.assetName },
           { title: "ASSET_DESCRIPTION", value: response?.description },
-          { title: "AST_DEPARTMENT", value: 'COMMON_MASTERS_DEPARTMENT_'+response?.department },
+          { title: "AST_DEPARTMENT", value: 'COMMON_MASTERS_DEPARTMENT_' + response?.department },
           { title: "AST_USAGE", value: response?.assetUsage },
+          { title: "AST_STATUS_ASSIGNABLE", value: response?.assetAssignable },
           { title: "AST_TYPE", value: response?.assetType },
 
         ],
@@ -113,7 +144,7 @@ console.log('first:= ', applicationDetails);
         title: "AST_DETAILS",
         asSectionHeader: true,
         values: getData(response, combinedData)
-        
+
       },
 
       // Conditionally include AST_ALLOCATION_DETAILS
@@ -157,29 +188,92 @@ console.log('first:= ', applicationDetails);
         },
       },
       {
-        title:"AST_Depriciation",
+        title: "AST_DEPRICIATION",
         asSectionHeader: true,
         isTable: true,
-        headers: [`${t("Start Date")}` , `${t("End Date")}`, "Depreciation Value", "Book Value", "Rate", "Old Book Value"],
+        headers: slotlistRows.length > 0
+          ? [
+            `${t("AST_START_DATE")}`,
+            `${t("AST_END_DATE")}`,
+            `${t("AST_DEPRECIATION_VALUE")}`,
+            `${t("AST_BOOK_VALUE")}`,
+            `${t("AST_RATE")}`,
+            `${t("AST_DEPRECIATION_METHOD")}`,
+            `${t("AST_OLD_BOOK_VALUE")}`
+          ]
+          : [`${t("AST_NO_DEPRECIATION_DATA")}`],
         tableRows: slotlistRows,
       },
+      {
+        title: "AST_MAINTENANCE",
+        asSectionHeader: true,
+        isTable: true,
+        isMaintenance: true,
+        headers: maintenanceListRows.length > 0
+          ? [
+            `${t("AST_MAINTENANCE_DATE")}`,
+            `${t("AST_VENDOR")}`,
+            `${t("AST_WARRANTY_STATUS")}`,
+            `${t("AST_COST_OF_MAINTENANCE")}`,
+            `${t("AST_MAINTENANCE_CYCLE")}`,
+            `${t("AST_MAINTENANCE_DOCUMENT")}`,
+            `${t("AST_MAINTENANCE_ACTION")}`
+          ]
+          : [`${t("AST_NO_MAINTENANCE_DATA")}`],
+        tableRows: maintenanceListRows,
+      },
+      {
+        title: "AST_DISPOSAL",  
+        asSectionHeader: true,
+        isTable: true,
+        headers: disposalListListRows.length > 0
+          ? [
+            `${t("AST_DISPOSAL_DATE")}`,
+            `${t("AST_REASON_DISPOSAL")}`,
+            `${t("AST_GL_CODE")}`,
+            `${t("AST_DISPOSAL_STATUS")}`,
+            `${t("AST_AGE_OF_ASSET")}`
+          ]
+          : [`${t("AST_NO_DISPOSAL_DATA")}`],
+        tableRows: disposalListListRows,
+      }
     ];
   },
   applicationDetails: async (t, tenantId, applicationNo, userType, combinedData, args) => {
-    
+
     const filter = { applicationNo, ...args };
     const response = await ASSETSearch.application(tenantId, filter);
+    // Fetch all data depriciation list
     const applicationDetails = await Digit.ASSETService.depriciationList({
-        Asset: {
+      Asset: {
         tenantId,
         id: response?.id,
         accountId: ""
-        }
-      });
-      
+      }
+    });
+    const maintenanceList = await Digit.ASSETService.maintenanceList({
+      AssetMaintenanceSearchCriteria: {
+        tenantId,
+        assetIds: [response?.id],
+        "limit": 10,
+        "offset": 0
+      }
+    });
+
+    // console.log('maintenanceList :- ',maintenanceList?.AssetMaintenance[0]?. documents)
+
+    const disposalList = await Digit.ASSETService.disposalList({
+      searchCriteria: {
+        tenantId,
+        assetIds: [response?.id],
+        limit: 10,
+        offset: 0
+      }
+    });
+    
     return {
       tenantId: response.tenantId,
-      applicationDetails: ASSETSearch.RegistrationDetails({ Assets: response, combinedData, t, applicationDetails }),
+      applicationDetails: ASSETSearch.RegistrationDetails({ Assets: response, combinedData, t, applicationDetails, maintenanceList, disposalList}),
       applicationData: response,
       transformToAppDetailsForEmployee: ASSETSearch.RegistrationDetails,
 
